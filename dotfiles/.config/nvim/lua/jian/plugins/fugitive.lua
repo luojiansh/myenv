08870c13a64e9f358820594e2d129538a4207ee3
Luo Jian <jian.luo.cn@gmail.com>
2024-04-12 13:13:25 +0200

    Fix git blame keybinding

--- dotfiles/.config/nvim/lua/jian/plugins/fugitive.lua
+++ dotfiles/.config/nvim/lua/jian/plugins/fugitive.lua
@@ -3,8 +3,8 @@ return {
         "tpope/vim-fugitive",
         cmd = "Git",
         keys = {
-            {"<leader>gs", vim.cmd.Git, desc="fugitive"},
-            {"<leader>gb", vim.cmd.Git, desc="git blame"}
+            {"<leader>gs", vim.cmd.Git, desc="git fugitive"},
+            {"<leader>gb", '<cmd>Git blame<cr>', desc="git blame"}
         }
     },
 }
