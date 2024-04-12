78997f4b1d58f50efca59e6a773cc657b8387c1d
Luo Jian <jian.luo.cn@gmail.com>
2024-04-12 13:02:08 +0200

    Fix haskell setup

--- dotfiles/.config/nvim/lua/jian/plugins/haskell-tools.lua
+++ dotfiles/.config/nvim/lua/jian/plugins/haskell-tools.lua
@@ -3,8 +3,8 @@ return {
         'mrcjkb/haskell-tools.nvim',
         dependencies = {
             'nvim-lua/plenary.nvim',
+            'mfussenegger/nvim-dap',
         },
         version = '^2', -- Recommended
-        ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
     }
 }
